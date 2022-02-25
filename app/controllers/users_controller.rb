ad4f5f49341fee06b84bc743e27e2b2b2e85a0af
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-25 10:45:22 -0500

    Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-    render json: @users.each{|user| user.attributes.except("password_digest")}
+     @users.each{|user| render json:user.attributes.except("password_digest")}
   end
 
   # GET /users/1
