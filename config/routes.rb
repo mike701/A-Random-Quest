9698ad4eaff187afd18ce645515485dcd6347a26
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-28 14:26:36 -0500

    friend

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get '/users/:user_id/posts', to: 'posts#get_user_posts'
   get '/comments', to: 'comments#get_all_comments'
   get '/users/:user_id/comments', to: 'comments#get_user_comments'
-  get '/users/:user_id/friends', to: 'users#friends'
+  get '/users/:id/friends', to: 'users#friends'
 
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
