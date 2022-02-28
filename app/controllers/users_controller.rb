35e3a3e2028363c69a4cfe47ed464020ed6c0f15
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-28 12:43:40 -0500

    friend array permit in user_controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:email, :username, :password,:follow)
+    params.require(:user).permit(:email, :username, :password,:friend)
   end
 
 
