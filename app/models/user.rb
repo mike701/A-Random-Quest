d067560897add0901265193f5eb8b00123ccdc54
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-28 16:01:33 -0500

    friend

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  serialize :friend
   #validates :password, length: { minimum: 6 }
 end
