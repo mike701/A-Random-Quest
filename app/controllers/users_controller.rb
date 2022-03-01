f43073cece04e7ac027460bdb826e2b4b5a38ee6
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-28 21:16:59 -0500

    friend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   end
 
   def addFriend
-    render json: params[:friendsList]
+    render json: @user["friendList"]=params[:friendsList]
     # if @user.update(params[:friendsList])
     #   render json: @user
     # else
--- client/src/Services/users.js
+++ client/src/Services/users.js
@@ -40,7 +40,7 @@ export const getUserFriends = async (user_id) => {
 }
 export const updateFriends = async (user_id, updateData) => {
   console.log(updateData.friend)
-  const resp = await api.put(`/users/${user_id}/friends/add`, {user:{friendsList: updateData}})
+  const resp = await api.put(`/users/${user_id}/friends/add`, {friendsList: updateData})
   return resp.data
 }
 export const getOneUserComments = async (user_id) => {
