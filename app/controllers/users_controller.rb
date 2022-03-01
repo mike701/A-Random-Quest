a338e9c772053f7a4b508d773f75f70dccb821a0
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-03-01 10:59:15 -0500

    friend join table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user.attributes.except("password"),  include: :posts
+    render json: @user.attributes.except("password"),  include: :posts,include: :friend
   end
 
   # POST /users
