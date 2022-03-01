9c3c9e38bf137f3c40fd620fb41f47e2320ea440
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-03-01 11:13:52 -0500

    friend join table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
 
   #get friends from /users/user_id/friends
   def friends
-    render json: @user, include: :friend
+    render json: @user.friend
   end
 
   def addFriend
