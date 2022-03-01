399c92b13c1bbe0e0ee3e2a682d1a3147028e90e
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-28 19:31:12 -0500

    friend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
   #get friends from /users/user_id/friends
   def friends
     @friends=@user.attributes.except("password_digest")
-   render json: @friends
+   render json: @friends["friend"]
   end
 
   def addFriend
