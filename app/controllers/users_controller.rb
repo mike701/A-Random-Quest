5010a95baad9701d13d682997d69698e4069597c
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-02-28 20:58:08 -0500

    friend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,12 +57,12 @@ class UsersController < ApplicationController
   end
 
   def addFriend
-    
-    if @user.update(params[:friendsList])
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    render json: param[:friendsList]
+    # if @user.update(params[:friendsList])
+    #   render json: @user
+    # else
+    #   render json: @user.errors, status: :unprocessable_entity
+    # end
   end
   private
   # Use callbacks to share common setup or constraints between actions.
