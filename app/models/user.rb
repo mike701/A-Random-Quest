224e8adde4ec665d017b48117dc43fb26dd0ac22
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-03-01 11:20:10 -0500

    friend join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
   has_many :comments, through: :post,dependent: :destroy
-  has_many :users, through: :friends
+  has_and_belongs_to_many :friends
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
